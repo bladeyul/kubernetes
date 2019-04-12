925a2690e3a699b0b3dc6f32669f6167659a9381
bladeyul <fryfish@126.com>
2019-04-12 15:17:35 +0800

    Update pull_dockers.sh

--- pull_dockers.sh
+++ pull_dockers.sh
@@ -1,5 +1,6 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:coredns_1.3.1
 docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:pause_3.1
+docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:flannel_v0.11.0-amd64
 docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-apiserver_v1.14.1
 docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-controller-manager_v1.14.1
 docker pull registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-scheduler_v1.14.1
@@ -13,6 +14,7 @@ docker tag registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-controller-m
 docker tag registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-scheduler_v1.14.1 k8s.gcr.io/kube-scheduler:v1.14.1
 docker tag registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-proxy_v1.14.1 k8s.gcr.io/kube-proxy:v1.14.1
 docker tag registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:etcd_3.3.10 k8s.gcr.io/etcd:3.3.10
+docker tag registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:flannel_v0.11.0-amd64 quay.io/coreos/flannel:v0.11.0-amd64
 
 docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:coredns_1.3.1
 docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:pause_3.1
@@ -21,3 +23,4 @@ docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-controller-m
 docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-scheduler_v1.14.1
 docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:kube-proxy_v1.14.1
 docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes:etcd_3.3.10
+docker rmi registry.cn-hangzhou.aliyuncs.com/bin_yu/kubernetes2:flannel_v0.11.0-amd64
